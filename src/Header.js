0748f38cedb4fc2aac9598ba21d61d5af0888ba3
zain-123 <35959511+zain-123@users.noreply.github.com>
2022-07-14 20:11:47 +0530

    Add files via upload

---
+++ src/Header.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './Header.css'
+import SearchIcon from "@mui/icons-material/Search";
+import LanguageIcon from "@mui/icons-material/Language";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { Avatar } from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+function Header() {
+    return (
+        <div className='header'>
+            <Link to='/'>
+                <img
+                    className="header__icon"
+                    src="https://i.pinimg.com/736x/ca/95/56/ca9556f097e9425a6298eb4953b8fbe1.jpg"
+                    alt=""
+                />
+            </Link>
+           
+            <div className='header__center'>
+                <input type="text" />
+                <SearchIcon />
+            </div>
+
+            <div className='header__right'>
+                <p>Become a host</p>
+                <LanguageIcon />
+                <ExpandMoreIcon />
+                <Avatar />
+            </div>
+        </div>
+    )
+}
+
+export default Header
